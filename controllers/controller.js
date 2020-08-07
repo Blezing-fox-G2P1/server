cfa49587113c517017828c3764db13c66808d340
agafp1996 <aga.febriawan@gmail.com>
2020-08-07 10:31:30 +0700

    added route and controller for edamam

--- controllers/controller.js
+++ controllers/controller.js
@@ -30,6 +30,41 @@ class Controller {
   static hitZomato(req, res, next) {
     
   }
+  static hitEdamam(req, res) {
+    try {
+
+        let input ={
+            //paramete url input yang dibutuhkan
+            quantity,
+            size,
+            food
+        }
+      axios({
+        method: 'GET',
+        //sample (bisa di tes) -> 'https://api.edamam.com/api/nutrition-data?app_id=d9f25735&app_key=947d762a17290cc043b8c781c5b41095&ingr=1%20large%20potato'
+        url: `https://api.edamam.com/api/nutrition-data?app_id=${process.env.EDAMAM_ID}&app_key=${process.env.EDAMAM_KEY}&ingr=${input.quantity}%20${input.size}%20${input.food}`
+      })
+      .then(result=>{
+
+        //belum dimanipulasi baru dimunculkan return datanya
+          let finalResult ={
+              calories,
+              totalWeight,
+              dietLabels,
+              healthLabels,
+              totalNutrients,
+          }
+
+          res.status(200).json(result.data)
+      })
+      .catch(err=>{
+        res.status(500).json(err)
+      })
+    } catch (err) {
+      res.status(500).json(err)
+    }
+
+  }
 }
 
 module.exports = Controller
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const Controller = require('../controllers/controller.js');
 
 router.get('/meals', authentication, Controller.hitMealDB)
 router.get('/restaurant', Controller.hitZomato)
+router.get('/nutrition', Controller.hitEdamam)
 
 module.exports = router;
